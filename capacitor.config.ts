3db30d17f60f07192d59bf08c338c9c64fc5d2da
George Catanescu <george.catanescu@student.tuiasi.ro>
2025-10-24 23:43:21 +0300

    icon update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.georgecatanescu.transitzen.app",
-  appName: "Transitzen",
+  appName: "ZeroWait",
   webDir: "dist",
   bundledWebRuntime: false,
   server: {
